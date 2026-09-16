8815f5e882a74a16925b455f70db97abf096d4c2
proctorsdp <proctorsdp@users.noreply.github.com>
2026-09-16 21:13:06 +0000

    deploy: proctorsdp/obsidian@bd9aac43bb10685e0de9238008195186611b1bf6

---
+++ postscript-360f2318.js
@@ -0,0 +1,16 @@
+await Promise.all([
+  import("./static/scripts/script-0-f7c52b6a.js"),
+  import("./static/scripts/script-1-0e41d61b.js"),
+  import("./static/scripts/script-2-f1365b3a.js"),
+  import("./static/scripts/script-3-4e3473f6.js"),
+  import("./static/scripts/script-4-8be19c3a.js"),
+  import("./static/scripts/script-5-cc8afa9d.js"),
+  import("./static/scripts/script-6-7e107d7e.js"),
+  import("./static/scripts/script-7-5ef560a8.js"),
+  import("./static/scripts/script-8-632b2822.js"),
+  import("./static/scripts/script-9-03272a32.js"),
+  import("./static/scripts/script-10-296b30c3.js"),
+  import("./static/scripts/script-11-be15df42.js"),
+  import("./static/scripts/script-12-a2f22c29.js")
+]);
+await import("./static/scripts/script-13-4a1444dc.js");
